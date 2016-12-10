e217ada3f40fcc9cf6e726ce9f4a199ff89ff4c6
Lenin Roa <lenin_513@hotmail.com>
2016-12-10 18:31:04 -0400

    Creacion de migraciones

--- database/migrations/2016_12_10_220909_create_productos_table.php
+++ database/migrations/2016_12_10_220909_create_productos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateProductosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('productos', function (Blueprint $table) {
             $table->increments('id');
             $table->string('nombre');
-            $table->string('apellido');
-            $table->string('telefono');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->decimal('precio');
+            $table->cantidad('integer')
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::dropIfExists('productos');
     }
 }
